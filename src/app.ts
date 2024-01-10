e33b7b85a79540e0bae819218858ac02fbf32d83
felipewww <felipewww@outlook.com.br>
2024-01-10 15:16:39 -0300

    core folder app settings

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,2 @@
-import 'module-alias/register'
-import 'dotenv/config'
-import * as express from 'express'
-import {exportTestModuleAlias} from "@/core/server";
-// import {exportTestModuleAlias} from "@/core/server";
-// import {exportTestModuleAlias} from "@/core/server";
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(process.env.APP_PORT, () => {
-    console.log(process.env.APP_ENV)
-    console.log(exportTestModuleAlias)
-    console.log(`Example app listening on port ${process.env.APP_PORT}`)
-})
+import './core/setup'
+import './core/server'
--- src/core/server.ts
+++ src/core/server.ts
@@ -1,1 +1,11 @@
-export const exportTestModuleAlias = 'hello module alias!'
+import * as express from "express";
+
+const app = express()
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Example app listening on port ${process.env.APP_PORT}`)
+})
+
+export {
+    app
+}
