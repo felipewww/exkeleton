ff6dad8f4f32b7fe01c114a397387cbf4f626668
felipewww <felipewww@outlook.com.br>
2024-01-10 14:54:07 -0300

    add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
+import 'dotenv/config'
 import * as express from 'express'
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(process.env.APP_PORT, () => {
+    console.log(process.env.APP_ENV)
+    console.log(`Example app listening on port ${process.env.APP_PORT}`)
 })
